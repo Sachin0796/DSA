aa0d0eb8cc16218ead66901105f8d9bf091cc4d0
sachin <msgtosachin@gmail.com>
2022-08-01 23:45:02 +0530

    Updated permutations

--- Recursion/permutations.py
+++ Recursion/permutations.py
@@ -1,3 +1,13 @@
-# find all the possible permutations of a given array
+# # find all the possible permutations of a given array
 
-# nhi smjh aaya code
+def permutations(arr, index, tempAns):
+    if index==len(arr):
+        return tempAns
+
+    for i in range(index,len(arr)):
+        arr[i], arr[index]=arr[index], arr[i]        
+        tempAns+=permutations(arr, index+1,tempAns)
+        arr[i], arr[index]=arr[index], arr[i]
+        return tempAns
+
+print(permutations(['123'],0,''))
