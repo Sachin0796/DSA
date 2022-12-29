3af255b732af72e8442ba09f867855368cef93ce
sachin <msgtosachin@gmail.com>
2022-12-30 00:17:23 +0530

    Changes in test and coinsCount

--- Recursion/test.py
+++ Recursion/test.py
@@ -1,10 +1,25 @@
-print(type([]))
-print(type([0].append(1)))
-print([].append(1))
+ansList=[]
+def findNumberSequence(direction):
+    ans=[0]*2
+    ans[0] = 0
+    ans[1] = 2**len(direction)
+    mydict=dict()     
+    index=0   
+    print(len(direction))
+    while index < len(direction):                          
+        tempAns=(ans[0] + ans[1])//2
+        
+        mydict[index] = tempAns
+        
+        if direction[index] == 'L':
+            ans[1] = tempAns
+        elif direction[index] == 'R':
+            ans[0] = tempAns        
+        index+=1                                
+    for i in sorted(mydict.values()):
+        ansList.append(list(mydict.values()).index(i)+1)
+    return ansList
 
+findNumberSequence('LLLL')
 
-a='abc'
-b='abcd'
-a=set(a)
-b=set(b)
-print(a in b)
+print(ansList)
